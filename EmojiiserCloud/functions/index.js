bc05438d10ed5671d459aa852a7a4beaea434174
BhaveyJain <jain385@purdue.edu>
2021-06-05 10:00:49 -0400

    Added cloud functionality by syncing firestore

--- EmojiiserCloud/functions/index.js
+++ EmojiiserCloud/functions/index.js
@@ -1,9 +1,20 @@
 const functions = require("firebase-functions");
 
+const admin = require("firebase-admin");
+admin.initializeApp();
+
 // // Create and Deploy Your First Cloud Functions
- //https://firebase.google.com/docs/functions/write-firebase-functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.addUserToFirestore = functions.auth.user().onCreate((user) => {
+  const usersRef = admin.firestore().collection("users");
+  return usersRef.doc(user.uid).set({
+    displayName: user.displayName,
+    emojis: "\u{1F61C}",
+  });
 });
