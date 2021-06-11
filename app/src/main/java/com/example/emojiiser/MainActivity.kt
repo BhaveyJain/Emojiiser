a8e9d349705c1c55fc2c86e115086927c5a7a7b8
BhaveyJain <jain385@purdue.edu>
2021-06-10 22:45:59 -0400

    Added testing ml functions

--- app/src/main/java/com/example/emojiiser/MainActivity.kt
+++ app/src/main/java/com/example/emojiiser/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.emojiiser
 
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.emojiiser.databinding.ActivityMainBinding
+import com.example.emojiiser.ml.Model
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import org.tensorflow.lite.support.image.TensorImage
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,12 +34,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+
+                val img =  BitmapFactory.decodeResource(resources, R.drawable.happily)
+                // data?.extras?.get("data") as Bitmap
+                val model = Model.newInstance(this)
+                // Creates inputs for reference.
+                val image = TensorImage.fromBitmap(img)
+
+                val outputs = model.process(image)
+                val probability = outputs.probabilityAsCategoryList
+
+                model.close()
+
+                Log.w(TAG, "$probability")
+                Log.w(TAG, "imgTakenSuccess")
+            }
+        }
 
         val view = binding.root
         setContentView(view)
@@ -50,8 +84,43 @@ class MainActivity : AppCompatActivity() {
             val logoutIntent = Intent(this, LoginActivity::class.java)
             logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(logoutIntent)
+        } else if (item.itemId == R.id.editIcon) {
+            showAlertDialog()
+        } else if (item.itemId == R.id.cameraIcon) {
+
+
+            val picIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultLauncher.launch(picIntent)
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog() {
+        val editText = EditText(this)
+
+        val dialog = AlertDialog.Builder(this)
+                .setTitle("Update your emojis")
+                .setView(editText)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", null)
+                .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            Log.i(TAG, "Clicked on positive button!")
+            val emojisEntered = editText.text.toString()
+            if (emojisEntered.isBlank()) {
+                Toast.makeText(this, "Cannot submit empty text", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val currentUser = auth.currentUser
+            if (currentUser == null) {
+                Toast.makeText(this, "No signed in user", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            db.collection("users").document(currentUser.uid)
+                    .update("emojis", emojisEntered)
+            dialog.dismiss()
+        }
     }
 }
