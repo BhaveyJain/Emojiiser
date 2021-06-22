fd9c7c7a94fb719c7310046be4c25da1ed67782b
BhaveyJain <jain385@purdue.edu>
2021-06-22 17:17:49 -0400

    Addedthe array of the emotions that the ML model can map to based on img

--- app/src/main/java/com/example/emojiiser/MainActivity.kt
+++ app/src/main/java/com/example/emojiiser/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.emojiiser
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -17,14 +20,20 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.emojiiser.databinding.ActivityMainBinding
-import com.example.emojiiser.ml.Model
+import com.example.emojiiser.ml.Model2
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
+import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
-import java.io.File
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,19 +55,39 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == Activity.RESULT_OK) {
                 // There are no request codes
                 val data: Intent? = result.data
+                var img  = BitmapFactory.decodeResource(resources, R.drawable.sadface);
+                img = convertBitMap(img)
+                img =  Bitmap.createScaledBitmap(img, 48, 48, true);
 
-                val img =  BitmapFactory.decodeResource(resources, R.drawable.happily)
+                val input = ByteBuffer.allocateDirect(48*48*4).order(ByteOrder.nativeOrder())
+
+                for (y in 0 until 48) {
+                    for (x in 0 until 48) {
+                        val px = img.getPixel(x, y)
+
+                        // Get channel values from the pixel value.
+                        val value = Color.red(px).toFloat() / 255.0f
+                        input.putFloat(value)
+
+                    }
+                }
+
                 // data?.extras?.get("data") as Bitmap
-                val model = Model.newInstance(this)
-                // Creates inputs for reference.
-                val image = TensorImage.fromBitmap(img)
-
-                val outputs = model.process(image)
-                val probability = outputs.probabilityAsCategoryList
-
+                val model = Model2.newInstance(this)
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 48, 48, 1), DataType.FLOAT32)
+                inputFeature0.loadBuffer(input)
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+                val arr = outputFeature0.floatArray
                 model.close()
+                Log.w(TAG, Arrays.toString(arr))
+                //[Angry, Disgust, Fear, Happy, Neutral, Sad, Surprised]
+                val unicodeList = listOf(String(Character.toChars(0x1F621)), String(Character.toChars(0x1F922)),
+                        String(Character.toChars(0x1F628)), String(Character.toChars(0x1F601)),
+                        String(Character.toChars(0x1F610)), String(Character.toChars(0x1F614)),
+                        String(Character.toChars(0x1F632)))
 
-                Log.w(TAG, "$probability")
+                updateStatus(unicodeList[detectedEmotion(arr)])
                 Log.w(TAG, "imgTakenSuccess")
             }
         }
@@ -77,6 +106,28 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    private fun detectedEmotion(arr: FloatArray): Int {
+        var ans: Int = 0;
+        var max: Float = arr[0]
+        for (i in arr.indices) {
+            if (arr[i] > max) {
+                max = arr[i]
+                ans = i
+            }
+        }
+        return ans
+    }
+
+    private fun updateStatus(emojisEntered: String) {
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            Toast.makeText(this, "No signed in user", Toast.LENGTH_SHORT).show()
+        } else {
+            db.collection("users").document(currentUser.uid)
+                    .update("emojis", emojisEntered)
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.miLogout) {
             Log.i(TAG,"User logged out" )
@@ -88,7 +139,6 @@ class MainActivity : AppCompatActivity() {
             showAlertDialog()
         } else if (item.itemId == R.id.cameraIcon) {
 
-
             val picIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(picIntent)
         }
@@ -96,6 +146,29 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun convertBitMap(bitmap: Bitmap) : Bitmap {
+        val finalMap = Bitmap.createBitmap(bitmap.width, bitmap.height, bitmap.config)
+        val height = bitmap.height
+        val width = bitmap.width
+
+        for (i in 0 until width) {
+            for (j in 0 until height) {
+
+                val colorPixel = bitmap.getPixel(i, j);
+                val A = Color.alpha(colorPixel);
+                var R = Color.red(colorPixel)
+                var G = Color.green(colorPixel)
+                var B = Color.blue(colorPixel)
+
+                R = (R + G + B) / 3
+                G = R
+                B = R
+                finalMap.setPixel(i, j, Color.argb(A, R, G, B))
+            }
+        }
+
+        return finalMap
+    }
     private fun showAlertDialog() {
         val editText = EditText(this)
 
@@ -113,13 +186,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Cannot submit empty text", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            val currentUser = auth.currentUser
-            if (currentUser == null) {
-                Toast.makeText(this, "No signed in user", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            db.collection("users").document(currentUser.uid)
-                    .update("emojis", emojisEntered)
+            updateStatus(emojisEntered)
             dialog.dismiss()
         }
     }
